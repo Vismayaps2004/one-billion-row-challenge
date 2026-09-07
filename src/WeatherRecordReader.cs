bcea630ed2a5464dedf8ce6df0df602b19d0afa9
Vismaya PS <Vismaya.PS@nationalgrid.com>
2026-09-07 17:22:20 +0530

    modifies WeatherRecorderReader

--- src/WeatherRecordReader.cs
+++ src/WeatherRecordReader.cs
@@ -2,26 +2,20 @@ namespace OneBillionRowChallenge;
 
 using System.IO;
 using System;
-public class WeatherRecordReader : IDisposable
+public class WeatherRecordReader : IDisposable, IAsyncDisposable
 {
-    private readonly FileStream stream;
-    private readonly byte[] buffer = new byte[1024 * 1024];
-    
+    private FileStream stream;
+    private byte[] buffer = new byte[1024 * 1024];
     private int bytesInBuffer;
     private int currentIndex;
     private int lineStart;
     
     public WeatherRecordReader(string filePath)
     {
-        stream = new FileStream(
-            filePath,
-            FileMode.Open,
-            FileAccess.Read,
-            FileShare.Read
-            );
+         stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
     }
 
-    public bool  TryReadRecord(out ReadOnlySpan<byte> record)
+    public bool TryReadRecord(out ReadOnlySpan<byte> record)
     {
         while (true)
         {
@@ -29,76 +23,73 @@ public class WeatherRecordReader : IDisposable
             if (currentIndex >= bytesInBuffer)
             {
                 bytesInBuffer = stream.Read(buffer);
-
                 if (bytesInBuffer == 0)
-                {
                     return false;
-                }
                 ResetParserState(0);
             }
-            if (parseBytes(out record))
+            if (ParseBytes(out record))
                 return true;
-            if (!readRemaining(out record))
+                
+            if (!ReadRemaining(out record))
                 return false;
         }
     }
 
-    private bool parseBytes(out ReadOnlySpan<byte> record)
+    private bool ReadRemaining(out ReadOnlySpan<byte> record)
     {
-        
-        for (int i = currentIndex; i < bytesInBuffer; i++)
-        {
-            if (buffer[i] == (byte)'\n')
-            {
-                record = buffer.AsSpan(lineStart, i - lineStart);
-                ResetParserState(i + 1);
-                return true;
-            }
-        }
-        currentIndex = bytesInBuffer;
         record = default;
-        return false;
-    }
-
-    private bool readRemaining(out ReadOnlySpan<byte> record)
-    {
         int remainingBytes = bytesInBuffer - lineStart;
         if (remainingBytes > 0)
         {
             buffer.AsSpan(lineStart, remainingBytes)
                 .CopyTo(buffer);
         }
-        
         int bytesRead = stream.Read(buffer.AsSpan(remainingBytes));
         bytesInBuffer = remainingBytes + bytesRead;
-        
-        if (bytesRead == 0) {
-            if (remainingBytes > 0) {
-                buffer.AsSpan(lineStart, remainingBytes)
-                    .CopyTo(buffer);
-                
+
+        if (bytesRead == 0)
+        {
+            if (remainingBytes > 0)
+            {
                 record = buffer.AsSpan(0, remainingBytes);
                 bytesInBuffer = 0;
                 return true;
             }
-
-            record = default;
             return false;
         }
         ResetParserState(0);
-
-        record = default;
         return true;
     }
 
+    private bool ParseBytes(out ReadOnlySpan<byte> record)
+    {
+        record = default;
+        for (int i = currentIndex; i < bytesInBuffer; i++)
+        {
+            if (buffer[i] == (byte) '\n')
+            { 
+                record = buffer.AsSpan(lineStart, i - lineStart);
+                ResetParserState(i + 1);
+                return true;
+            }
+        }
+        currentIndex = bytesInBuffer;
+        return false;
+    }
+    
     private void ResetParserState(int resetValue)
     {
-        currentIndex = resetValue;
         lineStart = resetValue;
+        currentIndex = resetValue;
     }
 
     public void Dispose()
     {
         stream.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await stream.DisposeAsync();
     }
 }
--- src/obj/Release/net10.0/src.AssemblyInfo.cs
+++ src/obj/Release/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8f98b7f579b74678394920ed3fa7e598240ea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f27afbcedc15670065354745309019271de63421")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
